6ba7a005e25daf88b7ee0aa03129bca7a33ecd29
KIONJanTannich <jan.tannich@kiongroup.com>
2026-08-07 16:25:23 +0200

    Port MKS-to-git export script from Python 2 to Python 3
    Fix print statements, bytes/str decoding, devpath sort, indentation,
    and the broken retargetsandbox os.system call.

--- mks_checkpoints_to_git.py
+++ mks_checkpoints_to_git.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 
 import os
@@ -9,35 +9,53 @@ import sys
 import re
 import platform
 from datetime import datetime
-# this is so windows doesn't output CR (carriage return) at the end of each line and just does LF (line feed)
-if platform.system() == 'Windows':
-    import msvcrt
-    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
 
-def export_data(string):
-    print 'data %d\n%s' % (len(string), string)
+# The git fast-import stream is written as raw bytes to stdout so that binary
+# file content is passed through unchanged and lines end in LF (not CRLF) on
+# Windows.
+_out = sys.stdout.buffer
 
-def inline_data(filename, code = 'M', mode = '644'):
-    content = open(filename, 'rb').read()
+
+def write_bytes(data):
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    _out.write(data)
+
+
+def export_data(data):
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+    write_bytes(b'data %d\n' % len(data))
+    write_bytes(data)
+    write_bytes(b'\n')
+
+
+def inline_data(filename, code='M', mode='644'):
+    with open(filename, 'rb') as handle:
+        content = handle.read()
+    display_name = filename
     if platform.system() == 'Windows':
-        #this is a hack'ish way to get windows path names to work git (is there a better way to do this?)
-        filename = filename.replace('\\','/')
-    print "%s %s inline %s" % (code, mode, filename)
+        # git fast-import expects forward slashes in path names
+        display_name = display_name.replace('\\', '/')
+    write_bytes('%s %s inline %s\n' % (code, mode, display_name))
     export_data(content)
 
+
 def convert_revision_to_mark(revision):
-    if not revision in marks:
+    if revision not in marks:
         marks.append(revision)
     return marks.index(revision) + 1
+
 
 def retrieve_revisions(devpath=0):
     if devpath:
         pipe = Popen('si viewprojecthistory --rfilter=devpath:"%s" --project="%s"' % (devpath, sys.argv[1]), shell=True, bufsize=1024, stdout=PIPE)
     else:
         pipe = Popen('si viewprojecthistory --rfilter=devpath::current --project="%s"' % sys.argv[1], shell=True, bufsize=1024, stdout=PIPE)
-    versions = pipe.stdout.read().split('\n')
+    output = pipe.stdout.read().decode('utf-8', errors='replace')
+    versions = output.split('\n')
     versions = versions[1:]
-    version_re = re.compile('[0-9]([\.0-9])+')
+    version_re = re.compile(r'[0-9]([\.0-9])+')
     revisions = []
     for version in versions:
         match = version_re.match(version)
@@ -49,45 +67,46 @@ def retrieve_revisions(devpath=0):
             revision["seconds"] = int(time.mktime(datetime.strptime(version_cols[2], "%b %d, %Y %I:%M:%S %p").timetuple()))
             revision["description"] = version_cols[5]
             revisions.append(revision)
-    revisions.reverse() # Old to new
+    revisions.reverse()  # Old to new
     re.purge()
     return revisions
 
+
 def retrieve_devpaths():
     pipe = Popen('si projectinfo --devpaths --noacl --noattributes --noshowCheckpointDescription --noassociatedIssues --project="%s"' % sys.argv[1], shell=True, bufsize=1024, stdout=PIPE)
-    devpaths = pipe.stdout.read()
-    devpaths = devpaths [1:]
-    devpaths_re = re.compile('    (.+) \(([0-9][\.0-9]+)\)\n')
+    devpaths = pipe.stdout.read().decode('utf-8', errors='replace')
+    devpaths = devpaths[1:]
+    devpaths_re = re.compile(r'    (.+) \(([0-9][\.0-9]+)\)\n')
     devpath_col = devpaths_re.findall(devpaths)
     re.purge()
-    devpath_col.sort(key=lambda x: map(int, x[1].split('.'))) #order development paths by version
+    devpath_col.sort(key=lambda x: [int(part) for part in x[1].split('.')])  # order development paths by version
     return devpath_col
 
-def export_to_git(revisions,devpath=0,ancestor=0):
+
+def export_to_git(revisions, devpath=0, ancestor=0):
     abs_sandbox_path = os.getcwd()
-	integrity_file = os.path.basename(sys.argv[1])
-    if not devpath: #this is assuming that devpath will always be executed after the mainline import is finished
+    integrity_file = os.path.basename(sys.argv[1])
+    if not devpath:  # this is assuming that devpath will always be executed after the mainline import is finished
         move_to_next_revision = 0
     else:
         move_to_next_revision = 1
     for revision in revisions:
-        #revision_col = revision["number"].split('\.')
         mark = convert_revision_to_mark(revision["number"])
         if move_to_next_revision:
-            os.system('si retargetsandbox --project="%s" --projectRevision=%s %s/%s' % (sys.argv[1], revision["number"], abs_sandbox_path), integrity_file)
+            os.system('si retargetsandbox --project="%s" --projectRevision=%s %s/%s' % (sys.argv[1], revision["number"], abs_sandbox_path, integrity_file))
             os.system('si resync --yes --recurse ')
         move_to_next_revision = 1
         if devpath:
-            print 'commit refs/heads/devpath/%s' % devpath
+            write_bytes('commit refs/heads/devpath/%s\n' % devpath)
         else:
-            print 'commit refs/heads/master'
-        print 'mark :%d' % mark
-        print 'committer %s <> %d +0100' % (revision["author"], revision["seconds"]) #Germany UTC time zone
+            write_bytes('commit refs/heads/master\n')
+        write_bytes('mark :%d\n' % mark)
+        write_bytes('committer %s <> %d +0100\n' % (revision["author"], revision["seconds"]))  # Germany UTC time zone
         export_data(revision["description"])
         if ancestor:
-            print 'from :%d' % convert_revision_to_mark(ancestor) #we're starting a development path so we need to start from it was originally branched from
-            ancestor = 0 #set to zero so it doesn't loop back in to here
-        print 'deleteall'
+            write_bytes('from :%d\n' % convert_revision_to_mark(ancestor))  # we're starting a development path so we need to start from what it was originally branched from
+            ancestor = 0  # set to zero so it doesn't loop back in to here
+        write_bytes('deleteall\n')
         tree = os.walk('.')
         for dir in tree:
             for filename in dir[2]:
@@ -103,17 +122,18 @@ def export_to_git(revisions,devpath=0,ancestor=0):
                     continue
                 inline_data(fullfile)
 
+
 marks = []
 devpaths = retrieve_devpaths()
 revisions = retrieve_revisions()
-#Create a build sandbox of the first revision
+# Create a build sandbox of the first revision
 os.system('si createsandbox --populate --recurse --project="%s" --projectRevision=%s tmp' % (sys.argv[1], revisions[0]["number"]))
 os.chdir('tmp')
-export_to_git(revisions) #export master branch first!!
+export_to_git(revisions)  # export master branch first!!
 for devpath in devpaths:
     devpath_revisions = retrieve_revisions(devpath[0])
-    export_to_git(devpath_revisions,devpath[0].replace(' ','_'),devpath[1]) #branch names can not have spaces in git so replace with underscores
-# Drop the sandbox
-shortname=sys.argv[1].replace('"', '').split('/')[-1]
+    export_to_git(devpath_revisions, devpath[0].replace(' ', '_'), devpath[1])  # branch names can not have spaces in git so replace with underscores
+# Drop the temporary "tmp" sandbox created above (does not touch any other sandbox)
+shortname = sys.argv[1].replace('"', '').split('/')[-1]
 os.chdir("..")
 os.system("si dropsandbox --yes -f --delete=all tmp/%s" % (shortname))
